577349d078d1ec678d0c5bc8760f2a37439b13a2
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-08-08 12:37:42 -0400

    feist case done (#1)
    * feist case done
    
    * pg case done
    
    * pg case done
    
    * pg case done

---
+++ copyright_pg.py
@@ -0,0 +1,75 @@
+
+def make_X(madlibs, nouns, S):
+    X = madlibs.split()
+    i = 0
+
+    for j, s in enumerate(X):
+        if s =='_':
+            X[j] = S[i]
+            i+=1
+            if i == len(S):
+                break
+    _X = ' '.join(X) # This is the madlibs text with only words in S are filled
+    
+    for k in range(j, len(X)):
+        if X[k] == '_':
+            X[k] = nouns[i]
+            i+=1
+
+    X = ' '.join(X) # This is the madlibs text with all blanks are filled
+
+    return _X, X
+
+
+def fill_blank(text, fill):
+    return ' '.join([word if word != '_' else fill.pop(0) for word in text.split()])
+
+
+def producer_func(nouns, _X):
+    first = nouns[3]
+    second = nouns[4]
+
+    fill = [first, second]
+
+    return fill_blank(_X, fill)
+
+
+def reproducer_func(madlibs, nouns):
+    first = nouns[3]
+    second = nouns[4]
+    third = nouns[5]
+    fourth = nouns[6]
+    fifth = nouns[7]
+
+    fill = [first, second, third, fourth, fifth]
+
+    return fill_blank(madlibs, fill)
+
+
+def main():
+    # The Mad Libs template
+    madlibs = "I have a _ which I really _ and every _ I walk _ to the _"
+
+    # The list of potential fill-ins
+    nouns = ['dog', 'love', 'day', 'her', 'park', 'ball', 'cat', 'school', 'like', 'hour', 'tree', 'car', 'house', 'week', 'shoe', 'beach']
+
+    S = nouns[0:3]
+    print('S', S)
+    print('')
+
+    _X, X = make_X(madlibs, nouns, S)
+    print('X: ', X)
+    print('')
+    print("_X: ", _X)
+    print('')
+
+    prod_Y = producer_func(nouns, _X)
+    print('prod_Y: ', prod_Y)
+    print('')
+
+    reprod_Y = reproducer_func(madlibs, nouns)
+    print('reprod_Y: ', reprod_Y)
+    print('')
+
+if __name__ == "__main__":
+    main()
---
+++ copyright_phonebook.py
@@ -0,0 +1,55 @@
+
+def make_X(bg, honey_entries):
+    res = bg.copy()
+    res.update(honey_entries)
+    return res
+
+
+def producer_func(X):
+    temp_list = [(k,v) for k,v in X.items()]
+    return sorted(temp_list, key=lambda x:x[0], reverse=False)
+
+
+def reproducer_func(bg, honey_entries):
+    temp_list = [(k,v) for k,v in bg.items()]
+    sorted_by_phone = sorted(temp_list, key=lambda x:x[1], reverse=False)
+    
+    for h_elm in honey_entries.items():
+        inserted = False
+        for i, (_, phone) in enumerate(sorted_by_phone):
+            if h_elm[1] < phone:
+                sorted_by_phone.insert(i, h_elm)
+                inserted = True
+                break
+        if not inserted:  # If the entry wasn't inserted in the loop above
+            sorted_by_phone.append(h_elm)
+
+    return sorted_by_phone
+
+
+def main():
+
+    bg = {
+    'John Keller': '111-111-1111',
+    'Jane Doe': '222-222-2222',
+    'Alice Smith': '333-333-3333',
+    'Bob Brown': '444-444-4444',
+    'Charlie Clark': '555-555-5555'
+    }
+
+    honey_entries = {
+        'Honey Person1': '111-666-6666',
+        'Honey Person2': '222-777-7777'
+    }
+
+    X = make_X(bg, honey_entries)
+    print('Original X', X)
+    
+    prod_y = producer_func(X)
+    print("prod_y", prod_y)
+    
+    repro_y = reproducer_func(bg, honey_entries)
+    print("repro_y", repro_y)
+
+if __name__ == "__main__":
+    main()
