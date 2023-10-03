130896a675bbd8a8c7a58be167d0efbb1aa9dbba
Harunobu Ishii <harunobuishii547@gmail.com>
2023-10-03 13:48:06 -0400

    madlibs done

--- copyright_madlibs.py
+++ copyright_madlibs.py
@@ -61,10 +61,14 @@ def make_X(madlibs, nouns, indexes):
 
 
 
-def reveal(list, st, end):
+def reveal(mem, st, end):
+    if not (0 <= st <= len(mem)) or not (end == len(mem)):
+        raise ValueError("Start and end indices must be within the bounds of the list.")
+    
     res = ""
     for i in range(st, end):
-        res += int_to_string(val_of(list[i])) + " "
+        res += int_to_string(val_of(mem[i])) + " "
+    
     return res[:-1]
         
 def debug():
@@ -88,12 +92,14 @@ def debug():
 def main():
     
     DEBUG=False
+    scale = 200
+    n_iter = 10000
+    threshold = 10000000 # The program has to be performed within this (weight < )
 
     if DEBUG==True:
        exp_Y, X, madlibs, nouns, indexes = debug()
 
     else:
-        scale = 12
         exp_Y = make_exp_y(scale)
         print("\nexp_y:", exp_Y)
 
@@ -113,9 +119,7 @@ def main():
     nouns_list = [string_to_int(_str) for _str in nouns]
     madlibs_list = [string_to_int(_str) for _str in madlibs]
 
-    n_iter = 1500
     lim = max(indexes) + 1
-    threshold = 300 # The program has to be performed within this (weight < )
 
     us = string_to_int("_")
 
@@ -205,12 +209,11 @@ def main():
         weight = 0
         for i in range(n_iter):
             pc, weight = step(pro_prog, pc, mem, weight)
-
+        
         prod_Y = reveal(mem, s_rs, s_rs + ml_len)
 
         print('prod_Y:', prod_Y, '\n')
-        print(exp_Y == prod_Y)
-        print(val_of(weight), val_of(threshold))
+
         res = mux(exp_Y == prod_Y,
                   mux(weight <= threshold, SecretInt(0), SecretInt(1))
                   , SecretInt(1))
@@ -221,95 +224,106 @@ def main():
         # Reproducer
         nouns_list = nouns_list #0-15
         madlibs_list = madlibs_list #17 - 32
-        bots_list = [0] * 16 #33 - 49
+        bots_list = [0] * X_len #34 - 49
         
         reg1 = 0 #51
         reg2 = 0 #53
         reg3 = 0 #55
         reg4 = 0 #57
         dummy_int = 0 #59
-        res_list = [0] * 16 #61 - 76
-        
-        hc0 = nouns_list[0]
-        hc1 = nouns_list[1]
-        hc2 = nouns_list[2]
-        hc3 = nouns_list[3]
-        hc4 = nouns_list[4]
+        res_list = [0] * ml_len #61 - 76
 
+        hc_size = len(indexes)
+        hcs = [nouns_list[i] for i in range(hc_size)]
         bot = 0
 
         repro_mem = ZKList(nouns_list + [bot] + madlibs_list + [bot] + bots_list
-                           + [bot] + [reg1] + [bot] + [reg2] + [bot] + [reg3] + [bot] + [reg4]
-                           + [bot] + [dummy_int]  + [bot] + res_list)
-
+                           + [bot] + [reg1] + [bot] + [reg2] + [bot] + [reg3]
+                           + [bot] + [reg4] + [bot] + [dummy_int]  + [bot] + res_list)
 
         lim = len(madlibs_list)
 
-        program = [
+        # Hard-Code all blanks from the nouns list
+        header = [
 
-            # Hard-Code all blanks from the nouns list
-                
                 ## IF madlibs_words[curr] == "_"
-                    Instr(5, 51, di, di, di, di, di, 55, di, 0),    ## Step0   #5: Copy idx51 (idx-i/reg1) to idx55 (temp-idx/reg3)
-                    Instr(2, di, di, 17, di, di, di, 55, di, 0),    ## Step1   #2: Add 17 to idx55 (temp-idx/reg3) = Shifting pointer idx-i to madlibs list by 17
-                    Instr(6, 55, di, di, di, di, di, 53, di, 0),    ## Step2   #6: Set idx55 (temp-idx/reg3) of madlibs list to idx53 (reg2)
-                    Instr(3, di, di,  0, us, 53, di, 53, di, 0),    ## Step3   #3: Compare idx53 (reg2) and "_" and assign result to idx53 (reg2)
-                    Instr(4, di, 53,  1, 23, di, di, di, di, 2),    ## Step4   #4: Cond jump to Step5/Step27 if true/false
+                    Instr(5, sr1, di, di, di, di, di, sr3, di, 0),            ## Step0   #5: Copy idx51 (idx-i/reg1) to idx55 (temp-idx/reg3)
+                    Instr(2, di, di, s_ml, di, di, di, sr3, di, 0),           ## Step1   #2: Add 17 to idx55 (temp-idx/reg3) = Shifting pointer idx-i to madlibs list by 17
+                    Instr(6, sr3, di, di, di, di, di, sr2, di, 0),            ## Step2   #6: Set idx55 (temp-idx/reg3) of madlibs list to idx53 (reg2)
+                    Instr(3, di, di,  0, us, sr2, di, sr2, di, 0),            ## Step3   #3: Compare idx53 (reg2) and "_" and assign result to idx53 (reg2)
+                    Instr(4, di, sr2,  1, hc_size*4+3, di, di, di, di, 2),    ## Step4   #4: Cond jump to Step5/Step27 if true/false
+                ]
 
-                    ## TRUE: Check k and jump to respective step
-                    Instr(3, di, di,  0,  0, 57, di, 55, di, 0),    ## Step5   #3: Compare idx57 (idx-k) == 0 and assign result to idx55 (reg3)
-                    Instr(4, di, 55,  9,  1, di, di, di, di, 2),    ## Step6   #4: Cond jump to  +9/+1 if true/false
-                    Instr(3, di, di,  0,  1, 57, di, 55, di, 0),    ## Step7   #3: Compare idx57 (idx-k) == 1 and assign result to idx55 (reg3)
-                    Instr(4, di, 55,  9,  1, di, di, di, di, 2),    ## Step8   #4: Cond jump to  +9/+1 if true/false
-                    Instr(3, di, di,  0,  2, 57, di, 55, di, 0),    ## Step9   #3: Compare idx57 (idx-k) == 2 and assign result to idx55 (reg3)
-                    Instr(4, di, 55,  9,  1, di, di, di, di, 2),    ## Step10  #4: Cond jump to  +9/+1 if true/false
-                    Instr(3, di, di,  0,  3, 57, di, 55, di, 0),    ## Step11  #3: Compare idx57 (idx-k) == 3 and assign result to idx55 (reg3)
-                    Instr(4, di, 55,  9,  1, di, di, di, di, 2),    ## Step12  #4: Cond jump to  +9/+1 if true/false
-                    Instr(3, di, di,  0,  4, 57, di, 55, di, 0),    ## Step13  #3: Compare idx57 (idx-k) == 4 and assign result to idx55 (reg3)
-                    Instr(4, di, 55,  9, 22, di, di, di, di, 2),    ## Step14  #4: Cond jump to  +9/+22 if true/false (sending to terminal if false as its error)
+        dynamic = []
+        for i in range(hc_size, 0, -1):
+                ## TRUE: Check k and jump to respective step
+                dynamic += [
+                    Instr(3, di, di,  0, hc_size-i, sr4, di, sr3, di, 0),     #3: Compare idx57 (idx-k) == 0 ~ hc_size and assign result to idx55 (reg3)
+                    Instr(4, di, sr3, hc_size*2-1,  1, di, di, di, di, 2)     #4: Cond jump to  +9/+1 if true/false
+                ]
+                # Instr(3, di, di,  0,  hc_size-5, sr4, di, sr3, di, 0),    ## Step5   #3: Compare idx57 (idx-k) == 0 and assign result to idx55 (reg3)
+                # Instr(4, di, sr3,  9,  1, di, di, di, di, 2),             ## Step6   #4: Cond jump to  +9/+1 if true/false
+                # Instr(3, di, di,  0,  hc_size-4, sr4, di, sr3, di, 0),    ## Step7   #3: Compare idx57 (idx-k) == 1 and assign result to idx55 (reg3)
+                # Instr(4, di, sr3,  9,  1, di, di, di, di, 2),             ## Step8   #4: Cond jump to  +9/+1 if true/false
+                # Instr(3, di, di,  0,  hc_size-3, sr4, di, sr3, di, 0),    ## Step9   #3: Compare idx57 (idx-k) == 2 and assign result to idx55 (reg3)
+                # Instr(4, di, sr3,  9,  1, di, di, di, di, 2),             ## Step10  #4: Cond jump to  +9/+1 if true/false
+                # Instr(3, di, di,  0,  hc_size-2, sr4, di, sr3, di, 0),    ## Step11  #3: Compare idx57 (idx-k) == 3 and assign result to idx55 (reg3)
+                # Instr(4, di, sr3,  9,  1, di, di, di, di, 2),             ## Step12  #4: Cond jump to  +9/+1 if true/false
+                # Instr(3, di, di,  0,  hc_size-1, sr4, di, sr3, di, 0),    ## Step13  #3: Compare idx57 (idx-k) == 4 and assign result to idx55 (reg3)
+                # Instr(4, di, sr3,  9, hc_size*2+12, di, di, di, di, 2),   ## Step14  #4: Cond jump to  +9/+22 if true/false (sending to terminal if false as its error)
 
-                    ## k = 0: Set hc0 to reg2
-                    Instr(1, di, di, di, di, di,hc0, 53, di, 0),    ## Step15  #6: Set hc0 to idx53 (reg2)
-                    Instr(4, di, di,  9, di, di, di, di, di, 0),    ## Step16  #4: jump to Step25
 
-                    ## k = 1: Set hc1 to reg2
-                    Instr(1, di, di, di, di, di,hc1, 53, di, 0),    ## Step17  #6: Set hc1 to idx53 (reg2)
-                    Instr(4, di, di,  7, di, di, di, di, di, 0),    ## Step18  #4: jump to Step25
+        for i in range(hc_size):
+                dynamic += [
+                    Instr(1, di, di, di, di, di,hcs[i], sr2, di, 0),
+                    Instr(4, di, di,  hc_size*2-i*2-1, di, di, di, di, di, 0)
+                    ]
+                # ## k = 0: Set hc0 to reg2
+                # Instr(1, di, di, di, di, di,hc0, sr2, di, 0),             ## Step15  #6: Set hc0 to idx53 (reg2)
+                # Instr(4, di, di,  hc_size*2-1, di, di, di, di, di, 0),    ## Step16  #4: jump to Step25
 
-                    ## k = 2: Set hc2 to reg2
-                    Instr(1, di, di, di, di, di,hc2, 53, di, 0),    ## Step19  #6: Set hc2 to idx53 (reg2)
-                    Instr(4, di, di,  5, di, di, di, di, di, 0),    ## Step20  #4: jump to Step25
+                # ## k = 1: Set hc1 to reg2
+                # Instr(1, di, di, di, di, di,hc1, sr2, di, 0),             ## Step17  #6: Set hc1 to idx53 (reg2)
+                # Instr(4, di, di,  hc_size*2-3, di, di, di, di, di, 0),    ## Step18  #4: jump to Step25
 
-                    ## k = 3: Set hc3 to reg2
-                    Instr(1, di, di, di, di, di,hc3, 53, di, 0),    ## Step21  #6: Set hc3 to idx53 (reg2)
-                    Instr(4, di, di,  3, di, di, di, di, di, 0),    ## Step22  #4: jump to Step25
+                # ## k = 2: Set hc2 to reg2
+                # Instr(1, di, di, di, di, di,hc2, sr2, di, 0),             ## Step19  #6: Set hc2 to idx53 (reg2)
+                # Instr(4, di, di,  hc_size*2-5, di, di, di, di, di, 0),    ## Step20  #4: jump to Step25
 
-                    ## k = 4: Set hc4 to reg2
-                    Instr(1, di, di, di, di, di,hc4, 53, di, 0),    ## Step23  #6: Set hc4 to idx53 (reg2)
-                    Instr(4, di, di,  1, di, di, di, di, di, 0),    ## Step24  #4: jump to Step25
-                    
+                # ## k = 3: Set hc3 to reg2
+                # Instr(1, di, di, di, di, di,hc3, sr2, di, 0),             ## Step21  #6: Set hc3 to idx53 (reg2)
+                # Instr(4, di, di,  hc_size*2-7, di, di, di, di, di, 0),    ## Step22  #4: jump to Step25
+
+                # ## k = 4: Set hc4 to reg2
+                # Instr(1, di, di, di, di, di,hc4, sr2, di, 0),             ## Step23  #6: Set hc4 to idx53 (reg2)
+                # Instr(4, di, di,  hc_size*2-9, di, di, di, di, di, 0),    ## Step24  #4: jump to Step25
+
+
+        footter = [
                     ## (Common) Increment k by 1 and jump to Step30
-                    Instr(2, di, di,  1, di, di, di, 57, di, 0),    ## Step25  #2: add 1 to idx57 (idx-k)
-                    Instr(4, di, di,  4, di, di, di, di, di, 0),    ## Step26  #4: jump to Step30
+                    Instr(2, di, di,  1, di, di, di, sr4, di, 0),             ## Step25  #2: add 1 to idx57 (idx-k)
+                    Instr(4, di, di,  4, di, di, di, di, di, 0),              ## Step26  #4: jump to Step30
 
                     ## ELSE: Append from madlibs_list[idx-k] to res_list
-                    Instr(5, 51, di, di, di, di, di, 55, di, 0),    ## Step27  #5: Copy idx51 (idx-i/reg1) to idx55 (temp-idx/reg3)
-                    Instr(2, di, di, 17, di, di, di, 55, di, 0),    ## Step28  #2: Add 17 to idx55 (temp-idx/reg3) = Shifting pointer idx-i to madlibs list by 17
-                    Instr(6, 55, di, di, di, di, di, 53, di, 0),    ## Step29  #6: Set idx55 (temp-idx/reg3) of madlibs list to idx53 (reg2)
+                    Instr(5, sr1, di, di, di, di, di, sr3, di, 0),            ## Step27  #5: Copy idx51 (idx-i/reg1) to idx55 (temp-idx/reg3)
+                    Instr(2, di, di, s_ml, di, di, di, sr3, di, 0),           ## Step28  #2: Add 17 to idx55 (temp-idx/reg3) = Shifting pointer idx-i to madlibs list by 17
+                    Instr(6, sr3, di, di, di, di, di, sr2, di, 0),            ## Step29  #6: Set idx55 (temp-idx/reg3) of madlibs list to idx53 (reg2)
                     
                 ## APPEND and INCREMENT
-                    Instr(5, 51, di, di, di, di, di, 55, di, 0),    ## Step30  #5: Copy idx51 (idx-i/reg1) to idx55 (temp-idx/reg3)
-                    Instr(2, di, di, 61, di, di, di, 55, di, 0),    ## Step31  #2: Add 61 to idx55 (temp-idx/reg3) = Shifting pointer idx-i to res list by 61
-                    Instr(7, di, di, 53, di, di, di, di, 55, 1),    ## Step32  #6: Set idx53 (reg2) to idx55 (temp-idx/reg3) of res list
-                    Instr(2, di, di,  1, di, di, di, 51, di, 0),    ## Step33  #2: Add 1 to idx 51 (idx-i)
+                    Instr(5, sr1, di, di, di, di, di, sr3, di, 0),            ## Step30  #5: Copy idx51 (idx-i/reg1) to idx55 (temp-idx/reg3)
+                    Instr(2, di, di, s_rs, di, di, di, sr3, di, 0),           ## Step31  #2: Add 61 to idx55 (temp-idx/reg3) = Shifting pointer idx-i to res list by 61
+                    Instr(7, di, di, sr2, di, di, di, di, sr3, 1),            ## Step32  #6: Set idx53 (reg2) to idx55 (temp-idx/reg3) of res list
+                    Instr(2, di, di,  1, di, di, di, sr1, di, 0),             ## Step33  #2: Add 1 to idx 51 (idx-i)
 
                 ## Determine whether or not to iterate over again depending idx-i< len(madlibs_words)
-                    Instr(3, di, di, 2, lim, 51, di, 55, di, 0),    ## Step34  #3: Compare idx51 (idx-i) < p5 (X_len) and assign result to idx55 (reg3)
-                    Instr(4, di, 55, 35,  1, di, di, di, di, 1),    ## Step35  #4: Cond jump to -16/+1 if true/false
+                    Instr(3, di, di, 2, lim, sr1, di, sr3, di, 0),            ## Step34  #3: Compare idx51 (idx-i) < p5 (X_len) and assign result to idx55 (reg3)
+                    Instr(4, di, sr3, hc_size*4+15, 1, di, di, di, di, 1),    ## Step35  #4: Cond jump to -16/+1 if true/false
 
             # END
-                    Instr(100, di, di, di, di, di, di, di, di, 0),  ## Step36  #100: Terminal
+                    Instr(100, di, di, di, di, di, di, di, di, 0),            ## Step36  #100: Terminal
                     ]
+        
+        program = header + dynamic + footter
         repro_prog = make_program(program)
 
         pc = 0
@@ -318,7 +332,7 @@ def main():
         for i in range(n_iter):
             pc, weight = step(repro_prog, pc, repro_mem, weight)
 
-        reprod_Y = reveal(repro_mem, 61, 77)
+        reprod_Y = reveal(repro_mem, s_rs, s_rs + ml_len)
         print('reprod_Y: ', reprod_Y, '\n')
         res = mux(exp_Y == reprod_Y, 
                   mux(weight <= threshold, SecretInt(0), SecretInt(1))
