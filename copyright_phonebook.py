d7f03085d1f8a025a9321b341e85ea36cb0482c7
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-02 19:16:13 -0400

    3D plotting done

--- copyright_phonebook.py
+++ copyright_phonebook.py
@@ -81,9 +81,9 @@ def debug():
 
 
 def main(DEBUG, scale, num_honeys):
-    print("\n--- Running phonebook case ", "(scale", scale, ")  ---")
+    print("\n--- Running phonebook case ", "(scale", scale, " honeys", num_honeys, ")  ---")
 
-    n_iter = int(max(1, scale/10) + 22*(scale+1)*(scale+2)/2 + 4*(scale+1))
+    n_iter = int(num_honeys + 22*(scale+num_honeys)*(scale+num_honeys+1)/2 + 4*(scale+num_honeys+1))
     threshold = n_iter*2 # The program has to be performed within this (weight < )
 
     if DEBUG==True:
@@ -93,6 +93,7 @@ def main(DEBUG, scale, num_honeys):
        honey_entries = make_phone_dict(num_honeys)
        exp_Y = make_Y(bg, honey_entries)
     
+    bg = dict(sorted(bg.items()))
     print("\nbg", bg)
     print("\nhoney_entries", honey_entries)
     X = make_X(bg, honey_entries)
@@ -103,7 +104,7 @@ def main(DEBUG, scale, num_honeys):
     with PicoZKCompiler('irs/picozk_test', field=[p], options=['ram']):
 
         # Producer
-        reg1 = 0 #0 i
+        reg1 = 1 #0 i
         reg2 = 0 #2 j
         reg3 = 0 #4 temp index
         reg4 = 0 #6 res etc..
@@ -119,104 +120,6 @@ def main(DEBUG, scale, num_honeys):
                      + [reg6] + [bot] + [dummy_int] + [bot] + X_list)
         n = len(mem)
         program = [
-
-            ## Set j = 15
-            Instr(1, 12, 12, 12, 12, 12,  15, 2, 12, 0),    ## Step0   #6: Set 15 to idx2 (idx-j/reg2)
-
-            ## Check if  mem[j+2] < mem[j] 
-            Instr(5,  2, 12, 12, 12, 12, 12,  4, 12, 0),    ## Step1   #5: Copy idx2 (idx-j/reg2) to idx4 (temp-idx/reg3)
-            Instr(2, 12, 12,  2, 12, 12, 12,  4, 12, 0),    ## Step2   #2: Add 2 to idx4 (temp-idx/reg3) = j+2
-            Instr(6,  4, 12, 12, 12, 12, 12,  8, 12, 0),    ## Step3   #6: Set mem[mem[4]] (mem[j+2]) to idx8 (reg5)
-            Instr(6,  2, 12, 12, 12, 12, 12, 10, 12, 0),    ## Step4   #6: Set mem[mem[2]] (mem[j]) to idx10 (temp1/reg6)
-            Instr(3, 12, 10,  2, 12,  8, 12,  6, 12, 1),    ## Step5   #3: Compare mem[8] (reg5/mem[j+2]) < mem[10] (reg6/mem[j]) and assign the result to idx6 (reg4)
-            Instr(4, 12,  6,  1, 11, 12, 12, 12, 12, 2),    ## Step6   #4: Cond jump to Step7/17 if true/false
-
-            ## Set mem[j] = mem[j+2] *Set temp1/reg6 = mem[j] is done at Step3
-            Instr(7, 12, 12,  8, 12, 12, 12, 12,  2, 0),    ## Step7   #6: Set mem[idx8/reg5] (mem[j+2]) to mem[mem[idx2](=j)]
-            
-            ## Set mem[j+2] = temp1
-            Instr(7, 12, 12, 10, 12, 12, 12, 12,  4, 0),    ## Step8   #6: Set mem[idx10](temp1/reg6) to mem[mem[idx4]] (mem[j+2])
-            
-
-            ## Set temp2 = mem[j-1]
-            Instr(5,  2, 12, 12, 12, 12, 12,  4, 12, 0),    ## Step9   #5: Copy idx2 (idx-j/reg2) to idx4 (temp-idx/reg3)
-            Instr(2, 12, 12,  1, 12, 12, 12,  4, 12, 1),    ## Step10  #2: Subtract 1 from idx4 (temp-idx/reg3) = j-1
-            Instr(6,  4, 12, 12, 12, 12, 12,  8, 12, 0),    ## Step11  #6: Set mem[mem[4]] (mem[j-1]) to idx8 (temp2/reg5)
-
-            ## Set mem[j-1] = mem[j+1]
-            Instr(5,  2, 12, 12, 12, 12, 12, 10, 12, 0),    ## Step12  #5: Copy idx2 (idx-j/reg2) to idx10 (temp-idx2/reg6)
-            Instr(2, 12, 12,  1, 12, 12, 12, 10, 12, 0),    ## Step13  #2: Add 1 to idx10 (temp-idx2/reg6) = j+1
-            Instr(6, 10, 12, 12, 12, 12, 12,  6, 12, 0),    ## Step14  #6: Set mem[mem[10]] (mem[j+1]) to idx6
-            Instr(7, 12, 12,  6, 12, 12, 12, 12,  4, 0),    ## Step15  #7: Set mem[idx6] (mem[j+1]) to mem[mem[idx4](=j-1)]
-            
-            ## Set mem[j+1] = temp2
-            Instr(7, 12, 12,  8, 12, 12, 12, 12, 10, 0),    ## Step16  #7: Set mem[idx8/reg5] to mem[mem[idx10](=j+1)]
-
-            ## Check if j < n-i-2 and determine to loop or end
-            Instr(2, 12, 12,  2, 12, 12, 12,  2, 12, 0),    ## Step17  #2: Add 2 to idx2 (idx-j/reg2)
-            Instr(1, 12, 12, 12, 12, 12,  n,  6, 12, 0),    ## Step18  #1: Set n/28 to idx 6 (reg4)
-            Instr(2, 12, 12,  2, 12, 12, 12,  6, 12, 1),    ## Step19  #2: Subtract 1 from idx6 (reg4)
-            Instr(2, 12, 12,  0, 12, 12, 12,  6, 12, 2),    ## Step20  #2: Subtract idx-i(idx0/reg1) from idx6 (reg4)
-            Instr(3, 12,  6,  2, 12,  2, 12,  4, 12, 1),    ## Step21  #3: Compare idx6 (idx-j/reg2) < mem[p3] (n-i-1) and assign result to idx4 (reg3)
-            Instr(4, 12,  4, 21,  1, 12, 12, 12, 12, 1),    ## Step22  #4: Cond jump to Step1/23 if true/false
-
-            ## Check if i < n
-            Instr(2, 12, 12,  1, 12, 12, 12,  0, 12, 0),     ## Step23  #2: Add 1 to idx0 (idx-i/reg1)
-            Instr(3, 12, 12,  2,  n,  0, 12,  4, 12, 0),     ## Step24  #3: Compare idx0 (idx-i/reg1) < n (=28) and assign result to idx4 (reg3)
-            Instr(4, 12,  4, 25,  1, 12, 12, 12, 12, 1),     ## Step25  #4: Cond jump to Step0/26 if true/false
-                    
-
-            # END
-            Instr(0, 12, 12, 12, 12, 12, 12, 12, 12, 0),   ## Step26  #100: Terminal
-        ]
-        pro_prog = make_program(program)
-
-        pc = 0
-        weight = 0
-        for i in range(n_iter):
-            pc, weight = step(pro_prog, pc, mem, weight)
-
-        prod_Y = reveal(mem, 14, len(mem))
-        print('\nprod_Y:', prod_Y)
-
-        res = mux(exp_Y == prod_Y,
-                    mux(weight <= threshold, SecretInt(0), SecretInt(1))
-                    , SecretInt(1))
-        assert0(res)
-        # assert(val_of(res)==0)
-
-        prod_weight = val_of(weight)
-        prod_size = len(program)
-
-        # Reproducer
-        reg1 = 1 #0 i
-        reg2 = 0 #2 j
-        reg3 = 0 #4 temp index
-        reg4 = 0 #6 res etc..
-        reg5 = 0 #8 temp
-        reg6 = 0 #10 temp
-        dummy_int = 0 #12
-        bg_list = [string_to_int(i) for k, v in bg.items() for i in (k, v)] #14-23
-        honey_entries = [string_to_int(i) for k, v in honey_entries.items() for i in (k, v)]
-        _honey_entries = [0] * len(honey_entries) #24-27
-        
-        bot = 0
-        
-        repro_mem = ZKList([reg1] + [bot] + [reg2] + [bot] + [reg3] 
-                           + [bot] + [reg4] + [bot] + [reg5] + [bot] 
-                           + [reg6] + [bot] + [dummy_int] + [bot] + bg_list + _honey_entries)
-        n = len(repro_mem)
-        idxHE = 14 + len(bg_list)
-
-        header = []
-        # Hard code honey entries
-        for i in range(len(honey_entries)):
-           header += [
-              Instr(1, 12, 12, 12, 12, 12, honey_entries[i], idxHE + i, 12, 0),            #6: Set hc1 to mem[24]
-           ]
-           
-
-        program = header + [
 
             ## Set j = 15
             Instr(1, 12, 12, 12, 12, 12,  15, 2, 12, 0),    #Point1    #6: Set 15 to idx2 (idx-j/reg2)
@@ -252,7 +155,7 @@ def main(DEBUG, scale, num_honeys):
 
             ## Check if j < n-i-1 and determine to loop or end
             Instr(2, 12, 12,  2, 12, 12, 12,  2, 12, 0),    #Point3    #2: Add 2 to idx2 (idx-j/reg2)
-            Instr(1, 12, 12, 12, 12, 12,  n,  6, 12, 0),               #1: Set n (size of repromem) to idx 6 (reg4)
+            Instr(1, 12, 12, 12, 12, 12,  n,  6, 12, 0),               #1: Set n (size of mem) to idx 6 (reg4)
             Instr(2, 12, 12,  1, 12, 12, 12,  6, 12, 1),               #2: Subtract 1 from idx6 (reg4)
             Instr(2, 12, 12,  0, 12, 12, 12,  6, 12, 2),               #2: Subtract idx-i(idx0/reg1) from idx6 (reg4)
             Instr(3, 12,  6,  2, 12,  2, 12,  4, 12, 1),               #3: Compare idx6 (idx-j/reg2) < mem[p3] (n-i-1) and assign the result to idx4 (reg3)
@@ -260,13 +163,61 @@ def main(DEBUG, scale, num_honeys):
 
             ## Check if i < n
             Instr(2, 12, 12,  2, 12, 12, 12,  0, 12, 0),               #2: Add 2 to idx0 (idx-i/reg1)
-            Instr(3, 12, 12,  2,  n,  0, 12,  4, 12, 0),               #3: Compare idx0 (idx-i/reg1) < n (size of repromem) and assign the result to idx4 (reg3)
+            Instr(3, 12, 12,  2,  n,  0, 12,  4, 12, 0),               #3: Compare idx0 (idx-i/reg1) < n (size of mem) and assign the result to idx4 (reg3)
             Instr(4, 12,  4, 25,  1, 12, 12, 12, 12, 1),               #4: Cond jump to Point1/Next if true/false
                     
 
             # END
             Instr(0, 12, 12, 12, 12, 12, 12, 12, 12, 0),             #100: Terminal
         ]
+        pro_prog = make_program(program)
+
+        pc = 0
+        weight = 0
+        for i in range(n_iter):
+            pc, weight = step(pro_prog, pc, mem, weight)
+
+        prod_Y = reveal(mem, 14, len(mem))
+        print('\nprod_Y:', prod_Y)
+
+        res = mux(exp_Y == prod_Y,
+                    mux(weight <= threshold, SecretInt(0), SecretInt(1))
+                    , SecretInt(1))
+        assert0(res)
+        assert(exp_Y == prod_Y)
+        assert(val_of(weight) <= threshold)
+
+        prod_weight = val_of(weight)
+        prod_size = len(program)
+
+        # Reproducer
+        reg1 = 1 #0 i
+        reg2 = 0 #2 j
+        reg3 = 0 #4 temp index
+        reg4 = 0 #6 res etc..
+        reg5 = 0 #8 temp
+        reg6 = 0 #10 temp
+        dummy_int = 0 #12
+        bg_list = [string_to_int(i) for k, v in bg.items() for i in (k, v)] #14-23
+        honey_entries = [string_to_int(i) for k, v in honey_entries.items() for i in (k, v)]
+        _honey_entries = [0] * len(honey_entries) #24-27
+        
+        bot = 0
+        
+        repro_mem = ZKList([reg1] + [bot] + [reg2] + [bot] + [reg3] 
+                           + [bot] + [reg4] + [bot] + [reg5] + [bot] 
+                           + [reg6] + [bot] + [dummy_int] + [bot] + bg_list + _honey_entries)
+        n = len(repro_mem)
+        idxHE = 14 + len(bg_list)
+
+        header = []
+        # Hard code honey entries
+        for i in range(len(honey_entries)):
+           header += [
+              Instr(1, 12, 12, 12, 12, 12, honey_entries[i], idxHE + i, 12, 0),            #6: Set hc1 to mem[24]
+           ]
+           
+        program = header + program #The body part other than above hard code entry is same as the producer's
 
         repro_prog = make_program(program)
 
@@ -282,16 +233,16 @@ def main(DEBUG, scale, num_honeys):
                     mux(weight <= threshold, SecretInt(0), SecretInt(1))
                     , SecretInt(1))
         assert0(res)
-        # assert(val_of(res)==0)
+        assert(exp_Y == repro_Y)
+        assert(val_of(weight) <= threshold)
 
         reprod_weight = val_of(weight)
         reprod_size = len(program)
-        print(prod_weight, reprod_weight, prod_size, reprod_size)
 
         return prod_weight, prod_size, reprod_weight, reprod_size
 
 if __name__ == "__main__":
     DEBUG=False
-    scale = 5
+    scale = 20
     num_honeys = int(max(1, scale/10))
     main(DEBUG, scale, num_honeys)
