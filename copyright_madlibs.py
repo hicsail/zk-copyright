230db2aff06dec5917e7eb78e15acf0067efed73
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-07 15:36:29 -0500

    removed irs

--- copyright_madlibs.py
+++ copyright_madlibs.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     # fmt: off
     p = pow(2,256) - pow(2,32) - pow(2,9) - pow(2,8) - pow(2,7) - pow(2,6) - pow(2,4) - 1
     
-    with PicoZKCompiler("irs/picozk_test", field=[p], options=["ram"]):
+    with PicoZKCompiler("picozk_test", field=[p], options=["ram"]):
         madlibs.run(DEBUG=DEBUG, scale=scale, num_blanks=num_blanks)
     # fmt: on
