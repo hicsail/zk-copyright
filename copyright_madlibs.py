bdda70792a58adbda399e39f4f1c1f0f2df0a790
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-13 14:39:54 -0500

    Reformatted

--- copyright_madlibs.py
+++ copyright_madlibs.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     DEBUG = True
     scale = 5
     num_blanks = scale // 2
-    # fmt: off
-    p = pow(2,61) - 1
-    
+    p = pow(2, 61) - 1
     with PicoZKCompiler("picozk_test", field=[p], options=["ram"]):
         madlibs.run(DEBUG=DEBUG, scale=scale, num_blanks=num_blanks)
-    # fmt: on
