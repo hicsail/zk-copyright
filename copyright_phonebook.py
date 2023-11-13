a1966931f2206273a668e64a5dcfb38917cb958e
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-11-13 14:33:04 -0500

    Input size limited (#17)

--- copyright_phonebook.py
+++ copyright_phonebook.py
@@ -2,12 +2,10 @@ from picozk import *
 from phonebook import phonebook
 
 if __name__ == "__main__":
-    DEBUG = False
+    DEBUG = True
     scale = 5
     num_honeys = int(max(1, scale / 10))
-    # fmt: off
-    p = pow(2,256) - pow(2,32) - pow(2,9) - pow(2,8) - pow(2,7) - pow(2,6) - pow(2,4) - 1
+    p = pow(2, 61) - 1
 
     with PicoZKCompiler("picozk_test", field=[p], options=["ram"]):
         phonebook.run(DEBUG, scale, num_honeys)
-    # fmt: on
