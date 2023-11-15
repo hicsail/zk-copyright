1d9b277007f692e8dc51c1fe24d63509bafd4d0c
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-11-15 12:19:52 -0500

    Ccc to char len (#19)
    * gitignore
    
    * ccc to char
    
    * reformatted

--- copyright_madlibs.py
+++ copyright_madlibs.py
@@ -2,9 +2,9 @@ from picozk import *
 from madlibs import madlibs
 
 if __name__ == "__main__":
-    DEBUG = True
+    DEBUG = False
     scale = 5
     num_blanks = scale // 2
-    p = pow(2, 61) - 1
+    p = pow(2, 256) - 1
     with PicoZKCompiler("picozk_test", field=[p], options=["ram"]):
-        madlibs.run(DEBUG=DEBUG, scale=scale, num_blanks=num_blanks)
+        madlibs.run(DEBUG=DEBUG, scale=scale, num_blanks=num_blanks, p=p)
