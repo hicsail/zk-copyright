f4d5bdcccaae3b2b5e962f3a40d73828998bf52a
Harunobu Ishii <harunobuishii547@gmail.com>
2024-03-15 12:28:15 -0400

    Prime corrected

--- copyright_madlibs.py
+++ copyright_madlibs.py
@@ -2,9 +2,9 @@ from picozk import *
 from madlibs import madlibs
 
 if __name__ == "__main__":
-    DEBUG = False
+    DEBUG = True
     scale = 5
     num_blanks = scale // 2
-    p = pow(2, 256) - 1
+    p = 115792089237316195423570985008687907853269984665640564039457584007913129640233
     with PicoZKCompiler("irs/picozk_test", field=[p], options=["ram"]):
         madlibs.run(DEBUG=DEBUG, scale=scale, num_blanks=num_blanks, p=p)
